49ed2a8616a202b8f2fa6bd5a3d38d985dcb0314
Chattrawut Phoolakorn <60160238@go.buu.ac.th>
2021-07-16 23:44:08 +0700

    make it better

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
     try:
         print(f"sending order {order_type} - {side} {quantity} {symbol}")
         #order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
-        order = client.futures_create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+        order = client.futures_create_order(symbol="ETHUSD", side=side, type=order_type, quantity=quantity)
     except Exception as e:
         print("an exception occured - {}".format(e))
         return False
