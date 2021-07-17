83d778357bbbfcbe094b975183a37cde83799d7a
Chattrawut Phoolakorn <60160238@go.buu.ac.th>
2021-07-17 17:44:32 +0700

    make it better

--- app.py
+++ app.py
@@ -18,12 +18,14 @@ def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
 
         buy_quantity = round(100 / float(coin_price['price']))
 
+        print(type(buy_quantity))
+
         print(coin_price['price'])
         print(respontext[1]['balance'])
         print(f"sending order {order_type} - {side} {quantity} {symbol}")
         
         #order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
-        total_price = int(coin_price['price'])
+        total_price = float(coin_price['price'])
         order = client.futures_create_order(symbol="BTCUSDT", side="BUY", type="LIMIT", price=total_price, quantity=buy_quantity, timeInForce=TIME_IN_FORCE_GTC,)
         
     except Exception as e:
