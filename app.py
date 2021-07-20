56e8ba0675a3740b880cf9f8d756c4372b9af583
Chattrawut Phoolakorn <60160238@go.buu.ac.th>
2021-07-20 20:17:02 +0700

    make it better

--- app.py
+++ app.py
@@ -9,34 +9,37 @@ API_KEY = '6041331240427dbbf26bd671beee93f6686b57dde4bde5108672963fad02bf2e'
 API_SECRET = '560764a399e23e9bc5e24d041bd3b085ee710bf08755d26ff4822bfd9393b11e'
 client = Client(API_KEY, API_SECRET,testnet=True)
 
+def check_position_status():
+    orders = client.futures_position_information(symbol="BTCUSDT")
+    if float(orders['positionAmt']) > 0: return True
+    else:  return False
+
 def open_position(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):  
     try:
         respontext = client.futures_account_balance()
-        candles = client.get_klines(symbol='BTCUSDT', interval=Client.KLINE_INTERVAL_4HOUR)
-        orders = client.futures_position_information(symbol="BTCUSDT")
-        print(orders)
+        #candles = client.get_klines(symbol='BTCUSDT', interval=Client.KLINE_INTERVAL_4HOUR)
         coin_price = client.get_symbol_ticker(symbol="BTCUSDT")
 
         amount = 100 / float(coin_price['price'])
         precision = 3
         buy_quantity = "{:0.0{}f}".format(amount, precision)
 
-        #buy_quantity = float(round(100 / float(coin_price['price']), 6))
-
-        print('your quantity', buy_quantity)
-        #print(symbol,' price is ',coin_price['price'])
-        print('your USDT', respontext[1]['balance'])
-        print(f"sending order {order_type} - {side} {quantity} {symbol}")
-        
-        #order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
-        
         amount2 = float(coin_price['price'])
         total_price = "{:0.0{}f}".format(amount2, precision)
+
+        position_status = check_position_status()
+        if position_status == True:
+            print("position has ready!")
+        else:
+            print("position has not ready!")
+
+        print('your USDT', respontext[1]['balance'])
         print(symbol,' price is ',total_price)
+        print('your quantity', buy_quantity)
+        print(f"sending order {order_type} - {side} {quantity} {symbol}")
 
-        order = client.futures_create_order(symbol="BTCUSDT", side="BUY", type="LIMIT", price=total_price, quantity=buy_quantity, timeInForce=TIME_IN_FORCE_GTC,)
+        order = client.futures_create_order(orderId="mainOrder",symbol="BTCUSDT", side="BUY", type="LIMIT", price=total_price, quantity=buy_quantity, timeInForce=TIME_IN_FORCE_GTC,)
 
-        
     except Exception as e:
         print("an exception occured - {}".format(e))
         return False
