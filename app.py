6122e51d4bdc68fde2f0ebdcecb8bd5464fa5e9a
Chattrawut Phoolakorn <60160238@go.buu.ac.th>
2021-07-20 16:37:25 +0700

    make it better

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ API_KEY = '6041331240427dbbf26bd671beee93f6686b57dde4bde5108672963fad02bf2e'
 API_SECRET = '560764a399e23e9bc5e24d041bd3b085ee710bf08755d26ff4822bfd9393b11e'
 client = Client(API_KEY, API_SECRET,testnet=True)
 
-def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
+def open_position(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):  
     try:
         respontext = client.futures_account_balance()
         candles = client.get_klines(symbol='BTCUSDT', interval=Client.KLINE_INTERVAL_4HOUR)
@@ -23,30 +23,39 @@ def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
 
         #buy_quantity = float(round(100 / float(coin_price['price']), 6))
 
-        print(type(buy_quantity))
-        print(buy_quantity)
-
-        print(coin_price['price'])
-        print(respontext[1]['balance'])
+        print('your quantity', buy_quantity)
+        #print(symbol,' price is ',coin_price['price'])
+        print('your USDT', respontext[1]['balance'])
         print(f"sending order {order_type} - {side} {quantity} {symbol}")
         
         #order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
         
         amount2 = float(coin_price['price'])
         total_price = "{:0.0{}f}".format(amount2, precision)
+        print(symbol,' price is ',total_price)
 
-        print(total_price)
-        if (orders['positionAmt']) == 0:
-            order = client.futures_create_order(symbol="BTCUSDT", side="BUY", type="LIMIT", price=total_price, quantity=buy_quantity, timeInForce=TIME_IN_FORCE_GTC,)
-        else:
-            return False
+        order = client.futures_create_order(symbol="BTCUSDT", side="BUY", type="LIMIT", price=total_price, quantity=buy_quantity, timeInForce=TIME_IN_FORCE_GTC,)
+
         
     except Exception as e:
         print("an exception occured - {}".format(e))
         return False
 
     return order
+def close_position():return 0 #เมื่อมีการชนเขต SLO หรือไม่เข้าออเดอร์ภายใน 5 แท่ง
 
+def open_stoploss():return 0
+def open_takeprofit():return 0
+
+def change_stoploss():return 0
+def change_takeprofit():return 0
+
+def close_order(): return 0 #สำหรับปิดออเดอร์ TP/SL เมือ position มีการเปลียนแปลง
+
+def every_1minute(): return 0 #เช็คสถานะราคาทุกๆนาที
+
+def fast_close_position(): return 0
+ 
 @app.route('/')
 def welcome():
     return render_template('index.html')
@@ -64,7 +73,7 @@ def webhook():
 
     side = data['strategy']['order_action'].upper()
     quantity = data['strategy']['order_contracts']
-    order_response = order(side, quantity, "DOGEUSDT")
+    order_response = open_position(side, quantity, "DOGEUSDT")
 
     if order_response:
         return {
