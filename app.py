147f5083cc46249913d30e5bcc57f7794f0b5ba2
Chattrawut Phoolakorn <60160238@go.buu.ac.th>
2021-07-23 03:21:50 +0700

    make it better

--- app.py
+++ app.py
@@ -48,6 +48,9 @@ def open_position(side, symbol, high, low, order_type=ORDER_TYPE_MARKET):
         tp2 = float(((float(high_price)*stoploss_percent*2)/100)+float(high_price))
         print(tp2)
 
+        tp3 = float(((float(high_price)*stoploss_percent*3)/100)+float(high_price))
+        print(tp3)
+
         quantity_tp = (float(quantity))/4
         quantity_tp = "{:0.0{}f}".format(quantity_tp, precision)
         print(quantity_tp)
@@ -79,7 +82,7 @@ def open_position(side, symbol, high, low, order_type=ORDER_TYPE_MARKET):
                 type="TAKE_PROFIT_MARKET",stopPrice=tp2, quantity=quantity_tp, timeInForce=TIME_IN_FORCE_GTC,)
 
                 order = client.futures_create_order(symbol=symbol, side="SELL", reduceOnly="true", closePosition="true",
-                type="TAKE_PROFIT_MARKET",stopPrice=tp2, quantity=quantity_tp2, timeInForce=TIME_IN_FORCE_GTC,)
+                type="TAKE_PROFIT_MARKET",stopPrice=tp3, quantity=quantity_tp2, timeInForce=TIME_IN_FORCE_GTC,)
 
                 order = client.futures_create_order(symbol=symbol, side="SELL", reduceOnly="true", closePosition="true",
                 type="STOP_MARKET",stopPrice=low_price, quantity=quantity, timeInForce=TIME_IN_FORCE_GTC,)
