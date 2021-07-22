bc9a9e5110c693902d2e7e0d62077dd79689ed3d
Chattrawut Phoolakorn <60160238@go.buu.ac.th>
2021-07-23 03:59:23 +0700

    make it better

--- app.py
+++ app.py
@@ -86,9 +86,9 @@ def open_position(side, symbol, high, low, order_type=ORDER_TYPE_MARKET):
                 type="TAKE_PROFIT_MARKET",stopPrice=tp3, quantity=quantity_tp2, timeInForce=TIME_IN_FORCE_GTC,)
 
                 order = client.futures_create_order(symbol=symbol, side="SELL", closePosition="true",
-                type="TAKE_PROFIT_MARKET",stopPrice=low_price, quantity=quantity, timeInForce=TIME_IN_FORCE_GTC,)
+                type="STOP_LOSS_MARKET",stopPrice=low_price, quantity=quantity, timeInForce=TIME_IN_FORCE_GTC,)
             else:
-                order = client.futures_create_order(symbol=symbol, side=side, type="STOP_MARKET",stopPrice=high_price, quantity=quantity, timeInForce=TIME_IN_FORCE_GTC,)
+                order = client.futures_create_order(symbol=symbol, side=side, type="LIMIT",stopPrice=high_price, quantity=quantity, timeInForce=TIME_IN_FORCE_GTC,)
                 
                 order = client.futures_create_order(symbol=symbol, side="BUY", reduceOnly="true",
                 type="TAKE_PROFIT_MARKET",stopPrice=tp1, quantity=quantity_tp, timeInForce=TIME_IN_FORCE_GTC,)
