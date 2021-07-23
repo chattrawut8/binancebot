398d056bc8717c941ab647eff52269be8a6cbb2b
Chattrawut Phoolakorn <60160238@go.buu.ac.th>
2021-07-23 16:36:27 +0700

    make it better

--- app.py
+++ app.py
@@ -49,14 +49,17 @@ def open_position(side, symbol, high, low, order_type=ORDER_TYPE_MARKET):
         if side == "BUY": tp1 = float(((float(high_price)*float(stoploss_percent))/100)+float(high_price))
         else: tp1 = float(float(high_price) - ((float(high_price)*float(stoploss_percent))/100))
         tp1 = "{:0.0{}f}".format(tp1, precision)
+        print(tp1)
 
         if side == "BUY": tp2 = float(((float(high_price)*float(stoploss_percent)*2)/100)+float(high_price))
         else: tp2 = float(float(high_price) - ((float(high_price)*float(stoploss_percent)*2)/100))
         tp2 = "{:0.0{}f}".format(tp2, precision)
+        print(tp2)
 
         if side == "BUY": tp3 = float(((float(high_price)*float(stoploss_percent)*3)/100)+float(high_price))
         else: tp3 = float(float(high_price) - ((float(high_price)*float(stoploss_percent)*2)/100))
         tp3 = "{:0.0{}f}".format(tp3, precision)
+        print(tp3)
 
         quantity_tp = (float(quantity))/4
         quantity_tp = "{:0.0{}f}".format(quantity_tp, precision)
