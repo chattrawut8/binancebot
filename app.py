a8b9b7516a09ae77c7bf62c411b5a458d243f688
Chattrawut Phoolakorn <60160238@go.buu.ac.th>
2021-07-24 01:15:39 +0700

    make it better

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def open_position(side, symbol, high, low, order_type=ORDER_TYPE_MARKET):
                 order = client.futures_create_order(symbol=symbol, side="SELL", closePosition="true",
                 type="TAKE_PROFIT_MARKET",stopPrice=low_price, quantity=quantity, timeInForce=TIME_IN_FORCE_GTC,)
             elif side == "SELL":
-                order = client.futures_create_order(symbol=symbol, side=side, type="LIMIT",price=high_price, quantity=quantity, timeInForce=TIME_IN_FORCE_GTC,)
+                order = client.futures_create_order(symbol=symbol, side=side, type="LIMIT",price=low_price, quantity=quantity, timeInForce=TIME_IN_FORCE_GTC,)
                 
                 order = client.futures_create_order(symbol=symbol, side="BUY", reduceOnly="true",
                 type="TAKE_PROFIT_MARKET",stopPrice=tp1, quantity=quantity_tp, timeInForce=TIME_IN_FORCE_GTC,)
@@ -105,7 +105,7 @@ def open_position(side, symbol, high, low, order_type=ORDER_TYPE_MARKET):
                 type="TAKE_PROFIT_MARKET",stopPrice=tp3, quantity=quantity_tp2, timeInForce=TIME_IN_FORCE_GTC,)
 
                 order = client.futures_create_order(symbol=symbol, side="BUY", closePosition="true",
-                type="TAKE_PROFIT_MARKET",stopPrice=low_price, quantity=quantity, timeInForce=TIME_IN_FORCE_GTC,)
+                type="TAKE_PROFIT_MARKET",stopPrice=high_price, quantity=quantity, timeInForce=TIME_IN_FORCE_GTC,)
         else:
             print('--- Order has ready can not open new order!!! ---')
             return False
