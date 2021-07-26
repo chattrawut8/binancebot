12132348fb1e800ce08fc236a2200a1ca6878273
Chattrawut Phoolakorn <60160238@go.buu.ac.th>
2021-07-27 06:15:00 +0700

    make it better

--- app.py
+++ app.py
@@ -56,10 +56,10 @@ def check_main_order_status(symbol):
 
 def save_orders_json(symbol):
     orders = client.futures_get_open_orders(symbol=symbol)
-    orders = sorted(orders, key=lambda x: x['stopPrice'])
+    sort_orders = sorted(orders, key=lambda x: x['stopPrice'])
 
     with open('orders.json', 'w') as outfile:
-        json.dump(orders, outfile)
+        json.dump(sort_orders, outfile)
     with open('orders.json', 'r') as openfile:
         json_object = json.load(openfile)
     print('\n' , 'total order ' , len(json_object))
