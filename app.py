543b36bfdeb7fee0ec7ae91c7abf516240a31f57
Chattrawut Phoolakorn <60160238@go.buu.ac.th>
2021-07-27 05:43:44 +0700

    make it better

--- app.py
+++ app.py
@@ -79,8 +79,8 @@ def check_hit_SL_TP(symbol):
         index = [x['reduceOnly'] for x in json_object].index(False)
         if json_object[index]['side'] == 'BUY':
             try:
-                client.futures_cancel_order(symbol=symbol, orderId=json_object[0]['orderId'])
-                #client.futures_cancel_order(symbol=symbol, orderId=json_object[index]['orderId'])
+                client.futures_get_order(symbol=symbol, orderId=json_object[0]['orderId'])
+                client.futures_get_order(symbol=symbol, orderId=json_object[index]['orderId'])
             except BinanceAPIException as e:
                 print('\n Has hit ST/TP BUY order!')
                 client = Client(API_KEY, API_SECRET)
@@ -88,8 +88,8 @@ def check_hit_SL_TP(symbol):
         else:
             len_orders = int(len(json_object)) - 1
             try:
-                client.futures_cancel_order(symbol=symbol, orderId=json_object[len_orders]['orderId'])
-                #client.futures_cancel_order(symbol=symbol, orderId=json_object[index]['orderId'])
+                client.futures_get_order(symbol=symbol, orderId=json_object[len_orders]['orderId'])
+                client.futures_get_order(symbol=symbol, orderId=json_object[index]['orderId'])
             except BinanceAPIException as e:
                 print('\n Has hit ST/TP SELL order!')
                 client = Client(API_KEY, API_SECRET)
